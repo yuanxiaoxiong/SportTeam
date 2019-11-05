c9617cfff68671777b8c31fca70f5668dc8ac059
kehuafu <1593238887@qq.com>
2019-11-06 00:45:33 +0800

    修复消息通知已知bug

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -168,9 +168,9 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function() {
-    let promise = tim.logout();
+    let promise = app.globalData.tim.logout();
     promise.then(function(imResponse) {
-      console.log(imResponse.data); // 登出成功
+      console.log(imResponse.data, "登出成功"); // 登出成功
     }).catch(function(imError) {
       console.warn('logout error:', imError);
     });
